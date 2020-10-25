0670c84480f88b5ffa8ece68f130fd9c2400e470
Arek Szymanski <arkadiuszsz@gmail.com>
2020-10-25 23:17:28 +0100

    a

--- src/main/java/MeasurementData.java
+++ src/main/java/MeasurementData.java
@@ -3,8 +3,23 @@ import java.time.format.DateTimeFormatter;
 public class MeasurementData {
 
 
-      EventListener
+        String pin = EventListener.pinNumber;
+        String state = String.valueOf(EventListener.pinState);
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        String dateTimeHigh= EventListener.dateTime.format(formatter)
+
+
+
+//Logika w listener – is high klasa stan ?
+//Update stanow czujnikow wyślij rest
+//{ „stanowisko”: „Pierwsze”, „czas”: 15.32 }
+//
+//Class Pomiar {
+//String stanowisko;
+//double czas’
+//}
 
 
 //        dateTime=LocalDateTime.now().format();
